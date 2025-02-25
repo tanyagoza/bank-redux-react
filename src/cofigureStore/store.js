cf2f744579b5e939fe55ba7ee5b90060315b12c4
tanya <tanja.goza@gmail.com>
2025-02-25 19:30:41 +0200

    original logger and thunk

--- src/cofigureStore/store.js
+++ src/cofigureStore/store.js
@@ -1,11 +1,12 @@
 import {applyMiddleware, legacy_createStore as createStore} from "redux";
 import {accountReducer} from "../reducer/accountReducer.js";
-import {loggerEnhancer} from "../enhancers/loggerEnhancer.js";
-import {thunkEnhancer} from "../enhancers/thunkEnhancer.js";
+import {thunk} from "redux-thunk";
+import logger from "redux-logger";
+
 
 const initialState = {
     balance: 0,
     quote: 'Winter is coming...',
 };
 
-export const store = createStore(accountReducer, initialState, applyMiddleware(thunkEnhancer, loggerEnhancer));
+export const store = createStore(accountReducer, initialState, applyMiddleware(thunk, logger));
